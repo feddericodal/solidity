26cd96feba30eea92d19a8369c465994c971300b
fed <federicodha@outlook.it>
2023-06-15 17:36:35 +0200

    questo e il primo cambiamento

---
+++ prova.js
@@ -0,0 +1,30 @@
+/*
+    prova codice
+*/
+const numero = 3; // questa variabile non puo essere modificata
+console.log("vediamo dove e l errore");
+
+if (numero == 3){
+        console.log("ciao");
+}
+function saluta(nome){
+    console.log("ciao: "+ nome);
+}
+saluta("federico");
+
+//////////oggetto
+
+const book = {
+    name:'libro',
+    autore: 'federico',
+    nonloso: 13,
+
+    getbook: function(){
+        return this.autore;
+    }
+}
+console.log(book.getbook());
+
+
+
+//////modifica a caso
